6447a241d405beb5df4ec69252e4fd3ce4df04f7
Luke Herrmann <herrmannluke@gmail.com>
2019-03-06 14:52:43 -0700

    Correction: Completed basic Obj's

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ $(document).ready( function(){
   $(".btn").on("click", function(){
     var choice = this.id
     var compChoice = rps[Math.floor(Math.random()*rps.length)]
+    
     if (choice == compChoice)
+      alert('You Tied. You picked: ' + choice + ' and the Computer picked: ' + compChoice)
+    if (choice == "paper" && compChoice == "rock" || choice == "rock" && compChoice == "scissors" || choice == "scissors" && compChoice == "paper")
       alert('You Win! You picked: ' + choice + ' and the Computer picked: ' + compChoice)
-    else (choice != compChoice)
-      alert('You loose... You picked: ' + choice + ' and the Computer picked: ' + compChoice)
+    if (compChoice == "paper" && choice == "rock" || compChoice == "rock" && choice == "scissors" || compChoice == "scissors" && choice == "paper")
+      alert('You lose... You picked: ' + choice + ' and the Computer picked: ' + compChoice)
 
   })
 })
